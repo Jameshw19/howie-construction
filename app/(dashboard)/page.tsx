6f1d59254d424a7cae9dfc7043e13d883545fd77
James <jameshowie17@outlook.com>
2024-03-06 15:38:30 +0000

    Updated styling in dashboard/page.tsx

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="bg-white p-12 lg:px-40 lg:py-20">
+    <main className="bg-white p-8 lg:px-48 lg:py-20">
       <div className="flex items-center justify-evenly">
         <div>
           <h2 className="text-lg lg:text-xl font-semibold text-[#225FAE]">
             HOWIES CONSTRUCTION
           </h2>
-          <h1 className="text-2xl lg:text-3xl font-bold">
+          <h1 className="text-4xl lg:text-5xl font-bold">
             Contact, Explore, or Learn <br /> about what we do.
           </h1>
           <h3 className="text-base lg:text-lg">
@@ -18,11 +18,11 @@ export default function Home() {
           </h3>
           <div className="py-4">
             <Button
-              className="text-lg bg-[#225FAE] rounded-md"
+              className="text-base bg-[#225FAE] rounded-md"
               variant="default"
               size="default"
             >
-              Contact
+              <span className="px-4">Contact</span>
             </Button>
           </div>
         </div>
