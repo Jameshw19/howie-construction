0abe4501ba6af1e04b136e6d5307a502ee0a911e
James <jameshowie17@outlook.com>
2024-03-06 14:06:41 +0000

    page.tsx for the landing... added decking.jpg and the text for the landing

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,3 +1,31 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <div>Dashboard Root Page</div>;
+  return (
+    <main className="bg-white px-40 py-20">
+      <div className="flex items-center justify-evenly">
+        <div>
+          <h2 className="text-lg font-semibold text-[#225FAE]">
+            HOWIES CONSTRUCTION
+          </h2>
+          <h1 className="text-3xl font-bold">
+            Contact, Explore, or Learn <br /> about what we do.
+          </h1>
+          <h3 className="text-base">
+            Elevate your outdoors with our <br /> Landscaping{" "}
+            <span className="font-bold text-[#225FAE] ">expertise</span>.
+          </h3>
+          <button className="py-2">Contact</button>
+        </div>
+        <div>
+          <Image
+            src="/decking.jpg"
+            alt="Decking Photo"
+            width={464}
+            height={464}
+          />
+        </div>
+      </div>
+    </main>
+  );
 }
