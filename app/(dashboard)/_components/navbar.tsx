107ab20754a8264605ae0c41095d5221ba1553bc
James <jameshowie17@outlook.com>
2024-03-06 14:14:20 +0000

    fixed navbar mobile styling and for lg devices. TODO: Same for the landing/page.tsx file

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-between py-10 px-40 border-b-2 bg-white border-[#225FAE]">
+    <div className="flex items-center justify-between p-5 lg:px-40 border-b-2 bg-white border-[#225FAE]">
       <div className="flex items-center space-x-4">
         <div className="flex items-center flex-col">
-          <Image src="/logo.svg" alt="Logo" height={200} width={200} />
-          <div className="text-xl p-2">0129255402</div>
+          <Image src="/logo.svg" alt="Logo" height={134} width={134} />
+          <div className="text-lg p-2">0129255402</div>
         </div>
       </div>
-      <ul className="flex items-center space-x-10">
+      <ul className="flex items-center space-x-4 lg:space-x-10">
         <li className="relative group">
           Home
           <div className="absolute inset-x-0 bottom-0 h-0.5 bg-transparent transition-all group-hover:bg-[#225FAE]"></div>
