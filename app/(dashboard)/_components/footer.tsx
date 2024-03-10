840e33d5a9cb292e3e664053945158ab043ce1de
James <jameshowie17@outlook.com>
2024-03-10 17:33:57 +0000

    Footer styling and layout

--- app/(dashboard)/_components/footer.tsx
+++ app/(dashboard)/_components/footer.tsx
@@ -1,19 +1,35 @@
+import { FaMapPin } from "react-icons/fa6";
+import { FaPhone } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
+
 const Footer = () => {
   return (
-    <div className="bg-[#225FAE] h-full text-white p-12">
+    <div className="bg-[#225FAE] h-full text-white p-10">
       <div className="flex items-center justify-evenly">
-        <div>
-          <h1 className="text-xl">Contact</h1>
-          <h2>📍Ayr, Scotland</h2>
-          <h2>📞012991290</h2>
-          <h2>✉️Email@test.com</h2>
+        <div className="flex justify-center items-center flex-col">
+          <h1 className="text-2xl">Contact</h1>
+          <div className="flex items-center justify-center">
+            <FaMapPin className="h-5 w-5" />
+            <h1>Ayr, Scotland</h1>
+          </div>
+          <div className="flex items-center justify-center">
+            <FaPhone className="h-5 w-5" />
+            <h1>012991290</h1>
+          </div>
+          <div className="flex items-center justify-center">
+            <IoMdMail className="h-5 w-5" />
+            <h1>Kris@kris.com</h1>
+          </div>
         </div>
-        <div>
-          <h1>Contact</h1>
-          <h2>Ayr</h2>
-          <h2>012991290</h2>
-          <h2>Email@test.com</h2>
+        <div className="flex justify-center items-center flex-col">
+          <h1 className="text-2xl">Locations</h1>
+          <h1>Ayrshire</h1>
+          <h1>Glasgow</h1>
+          <h1>Renfrewshire</h1>
         </div>
+      </div>
+      <div className="flex items-center justify-center mt-8 text-base">
+        Copyright &copy; 2024 Howies Construction
       </div>
     </div>
   );
